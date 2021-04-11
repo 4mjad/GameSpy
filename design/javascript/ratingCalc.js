f1975f373b5e3e8474f0317f3ef1dbadb669bf24
Omer Guven <oguve001@gold.ac.uk>
2021-04-11 21:14:41 +0100

    26th (11/04/2021) commit

---
+++ design/javascript/ratingCalc.js
@@ -0,0 +1,21 @@
+// function GetRating( positiveVotes, negativeVotes ) {
+//     const totalVotes = positiveVotes + negativeVotes;
+//     const average = positiveVotes / totalVotes;
+//     const score = average - ( average - 0.5 ) * Math.pow( 2, -Math.log10( totalVotes + 1 ) );
+//     console.log(score)
+//     return score * 100;
+// }
+
+
+
+// ((positiveVotes / totalVotes) - ((positiveVotes / totalVotes) - 0.5) * Math.pow(2, -Math.log10( (positiveVotes + negativeVotes)  + 1 ))) * 100
+
+
+
+
+
+
+
+
+
+

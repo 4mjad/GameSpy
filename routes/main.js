e853c895796c739734adc4fdba9724611180f671
Omer Guven <oguve001@gold.ac.uk>
2021-04-21 17:56:06 +0100

    32th commit

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ module.exports = function (app) // this file has been exported as a function
   const { check, validationResult } = require('express-validator');
   //redirect to login pages if the user have not logged in
   const redirectLogin = (req, res, next) => {
-    
+
     if (!req.session.userId) {
       res.redirect('./login')
     }
@@ -29,9 +29,9 @@ module.exports = function (app) // this file has been exported as a function
       var db = client.db('GameSpy'); // name of db
       db.collection('ALL').find({ name: { $regex: new RegExp(req.query.keyword, "i") } }).toArray((findErr, results) => { // finds name of books with specific key words
         if (findErr) throw findErr // outputs error
-        else{
+        else {
           res.render('gamepage.ejs', { gametitle: req.query.keyword, availablegame: results }); // otherwise produce output
-        client.close(); // closes all open connections
+          client.close(); // closes all open connections
         }
       });
     });
@@ -74,7 +74,7 @@ module.exports = function (app) // this file has been exported as a function
             username: req.sanitize(req.body.username),
             password: hashedPassword
           });
-          res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' +  'You are now registered, Your user name is: ' + req.body.username + ' your password is: ' + req.body.password + ' and your hashed password is: ' + hashedPassword + '</p>'  + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>');
+          res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + 'You are now registered, Your user name is: ' + req.body.username + ' your password is: ' + req.body.password + ' and your hashed password is: ' + hashedPassword + '</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>');
           client.close();
         })
       }
@@ -82,7 +82,7 @@ module.exports = function (app) // this file has been exported as a function
   });
 
 
-//////////////////////------------------------------- LOGIN PAGE -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
+  //////////////////////------------------------------- LOGIN PAGE -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
 
   app.get('/login', function (req, res) {
     res.render('login.html')
@@ -117,31 +117,31 @@ module.exports = function (app) // this file has been exported as a function
               if (result == true) {
                 req.session.userId = req.sanitize(req.body.username);
                 //if the provided username and password are correct the user session starts 
-                res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('Logged In, All information provided is correct') + '</p>'  + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
+                res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('Logged In, All information provided is correct') + '</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
               }
               else {
                 //if username is correct and the password wrong, then the following line prints out
-                res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('Invalid Password Entered') +'</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
+                res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('Invalid Password Entered') + '</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
               }
             })
           }
           else {
             //if provided user name is not in the database then the following line prints out
-            res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('Invalid Username Entered') +'</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
+            res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('Invalid Username Entered') + '</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
           }
         })
       }
     })
   });
 
-//////////////////////------------------------------- LOGOUT -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
+  //////////////////////------------------------------- LOGOUT -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
 
   app.get('/logout', redirectLogin, (req, res) => {
     req.session.destroy(err => {
       if (err) {
         return res.redirect('./')
       }
-      res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('you are now logged out.') + '</p>'  + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>');
+      res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('you are now logged out.') + '</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>');
     })
   })
 
@@ -172,7 +172,7 @@ module.exports = function (app) // this file has been exported as a function
           Subject: req.body.subject,
           Message: req.body.message,
         });
-        res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ("Your Email is: " + req.body.email + "The subject of the issue is: " + req.body.subject + "The message you are sending is: " + req.body.message) + '</p>'  + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>');  //sends response to user
+        res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ("Your Email is: " + req.body.email + "The subject of the issue is: " + req.body.subject + "The message you are sending is: " + req.body.message) + '</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>');  //sends response to user
         client.close();
       }
     })
@@ -180,61 +180,61 @@ module.exports = function (app) // this file has been exported as a function
 
 
 
-//////////////////////------------------------------- COMPLAINTS PAGE FOR ADMIN -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
-//only accessable by admins
-app.get('/complaintsadmin',redirectLogin,function(req,res) {
-  var MongoClient = require('mongodb').MongoClient;
-  const url = "mongodb+srv://GameSpy:gamespy123@gamespy.inxg2.mongodb.net/test";
-  MongoClient.connect(url, function (err, client) {
-  if (err) throw err;
-  var db = client.db('GameSpy');
-  db.collection('Complaints').find().toArray((findErr, results) => { // produces all available books
-  if (findErr) throw findErr;
-  else
-     //web page with all active complaints
-     res.render('complaintsadmin.ejs', {availablegame:results});
-  client.close();
-});
-});
-})
-
-app.post('/deleted',[check('email').isEmail()],function(req,res){
-  //saving data in database
-  var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb+srv://GameSpy:gamespy123@gamespy.inxg2.mongodb.net/test";
-
-  MongoClient.connect(url,function(err,client){
-    if(err) throw err;
-    var db = client.db('GameSpy');  //access to the database
-    //gets the email address of the user
-    var word1 = req.body.email;
-    console.log(word1);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.redirect('./complaints');
-    }
-    else {
-      //if the user email is found, then the complaint is delete
-    db.collection('Complaints').findOne({Email:word1},function(err,result){ //find if the username entered is in the database if so it would get all the information for that user
-    if(err) throw err;
-    if(result != null){ //if result is not equal to null that means the username exists in the database so it would execute the code within the if statement
-
-      db.collection('Complaints').deleteOne({Email:word1},function(err,results){ //delete the user that has the username entered in the users collection by comparing username entered with the username in the collection and then delete all of that users information from the database and then tell the user the information is deleted
-        res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' +('Deleting User ' + word1 + ' from the database') + '</p>' + '<br />'+'<a href='+'./'+'>Home</a>') 
-      })
-    }
-    else{ //if username is not found in the database that means results is equal to null then tell the user that the username entered does not exist in the database
-       res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style = "color:#FFFFFF;">' + ("User with the username " + word1 + " does not exist in the database") +'</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
-    }
-    })
-  }
+  //////////////////////------------------------------- COMPLAINTS PAGE FOR ADMIN -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
+  //only accessable by admins
+  app.get('/complaintsadmin', redirectLogin, function (req, res) {
+    var MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb+srv://GameSpy:gamespy123@gamespy.inxg2.mongodb.net/test";
+    MongoClient.connect(url, function (err, client) {
+      if (err) throw err;
+      var db = client.db('GameSpy');
+      db.collection('Complaints').find().toArray((findErr, results) => { // produces all available books
+        if (findErr) throw findErr;
+        else
+          //web page with all active complaints
+          res.render('complaintsadmin.ejs', { availablegame: results });
+        client.close();
+      });
+    });
   })
-}); 
+
+  app.post('/deleted', [check('email').isEmail()], function (req, res) {
+    //saving data in database
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb+srv://GameSpy:gamespy123@gamespy.inxg2.mongodb.net/test";
+
+    MongoClient.connect(url, function (err, client) {
+      if (err) throw err;
+      var db = client.db('GameSpy');  //access to the database
+      //gets the email address of the user
+      var word1 = req.body.email;
+      console.log(word1);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.redirect('./complaints');
+      }
+      else {
+        //if the user email is found, then the complaint is delete
+        db.collection('Complaints').findOne({ Email: word1 }, function (err, result) { //find if the username entered is in the database if so it would get all the information for that user
+          if (err) throw err;
+          if (result != null) { //if result is not equal to null that means the username exists in the database so it would execute the code within the if statement
+
+            db.collection('Complaints').deleteOne({ Email: word1 }, function (err, results) { //delete the user that has the username entered in the users collection by comparing username entered with the username in the collection and then delete all of that users information from the database and then tell the user the information is deleted
+              res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ('Deleting User ' + word1 + ' from the database') + '</p>' + '<br />' + '<a href=' + './' + '>Home</a>')
+            })
+          }
+          else { //if username is not found in the database that means results is equal to null then tell the user that the username entered does not exist in the database
+            res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style = "color:#FFFFFF;">' + ("User with the username " + word1 + " does not exist in the database") + '</p>' + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
+          }
+        })
+      }
+    })
+  });
 
 
-//////////////////////------------------------------- API PAGE -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
+  //////////////////////------------------------------- API PAGE -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
 
-app.get('/api', function (req, res) {
+  app.get('/api', function (req, res) {
     var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb+srv://GameSpy:gamespy123@gamespy.inxg2.mongodb.net/test";
     MongoClient.connect(url, function (err, client) {
@@ -243,50 +243,50 @@ app.get('/api', function (req, res) {
       db.collection('ALL').find().toArray((findErr, results) => {
         if (findErr) throw findErr;
         else
-        //shows the games as a json file
+          //shows the games as a json file
           res.json(results);
       });
     });
   });
 
 
-//////////////////////------------------------------- FORUM PAGE -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
+  //////////////////////------------------------------- FORUM PAGE -----------------------------------\\\\\\\\\\\\\\\\\\\\\\\\
 
-  app.get('/forum',redirectLogin,function(req,res) {
+  app.get('/forum', redirectLogin, function (req, res) {
     var MongoClient = require('mongodb').MongoClient;
     const url = "mongodb+srv://GameSpy:gamespy123@gamespy.inxg2.mongodb.net/test";
     MongoClient.connect(url, function (err, client) {
-    if (err) throw err;
-    var db = client.db('GameSpy');
-    db.collection('Messages').find().sort({ TimeDate: -1 }).toArray((findErr, results) => { 
-    if (findErr) throw findErr;
-    else
-       //prints the messages into a webpage
-       res.render('forum.ejs', {availablemessage:results});
-    client.close();
-  });
-  });
+      if (err) throw err;
+      var db = client.db('GameSpy');
+      db.collection('Messages').find().sort({ TimeDate: -1 }).toArray((findErr, results) => {
+        if (findErr) throw findErr;
+        else
+          //prints the messages into a webpage
+          res.render('forum.ejs', { availablemessage: results });
+        client.close();
+      });
+    });
   })
-  
-  app.post("/MessageAdded",(req,res)=>{
+
+  app.post("/MessageAdded", (req, res) => {
     var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb+srv://GameSpy:gamespy123@gamespy.inxg2.mongodb.net/test";
-    var date= new Date();
-    
-    MongoClient.connect(url, (err, client)=> {
-    if(err) throw err;
-    var db = client.db('GameSpy');
-    //enters the message and all the datas into the database Messages
-    db.collection("Messages").insertOne({
-      username: req.session.userId, 
-      message:req.body.message, 
-      TimeDate:date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear() +' ('+date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() +') '
-    }, function(err,result){
-    if(err) throw err;
-    res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ("Hello " + req.session.userId +  " You have submitted the following message: " + req.body.message + '</p>') + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
+    var date = new Date();
 
-    });
-    client.close(); 
+    MongoClient.connect(url, (err, client) => {
+      if (err) throw err;
+      var db = client.db('GameSpy');
+      //enters the message and all the datas into the database Messages
+      db.collection("Messages").insertOne({
+        username: req.session.userId,
+        message: req.body.message,
+        TimeDate: date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + ' (' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ') '
+      }, function (err, result) {
+        if (err) throw err;
+        res.send('<link rel="stylesheet" type="text/css" href="css/custom.css">' + '<p style= "color:#FFFFFF;">' + ("Hello " + req.session.userId + " You have submitted the following message: " + req.body.message + '</p>') + '<br />' + '<a href=' + './' + '>Home </a>' + '<a href=' + './PopularGames' + '> PopularGames </a> ' + '<a href=' + './Complaints' + '> Complaints</a>' + '<a href=' + './Forum' + '> Forum </a>' + '<a href=' + './login' + '> login </a>' + '<a href=' + './logout' + '> Logout</a>')
+
+      });
+      client.close();
     });
 
   })

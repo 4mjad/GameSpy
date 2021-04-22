4bde50b0a49abcd02cdf3078e0ab497863830482
Omer Guven <oguve001@gold.ac.uk>
2021-04-22 16:05:56 +0100

    33rd commit

--- design/javascript/Linear.js
+++ design/javascript/Linear.js
@@ -40,26 +40,28 @@ function Linear(price){
         }
         //equation for slope
         slope = x2/y2;
-        //to find the constant for the equation of a straight line
+        //to find the constant for the equation of the straight line
         constant = y1/(slope * x1);
 
         }
    
-
+         
         // graph for the scatter plot for the prices using d3 library
+        // https://gramener.github.io/d3js-playbook/scatter.html
         function graph(b,c){
-
+            //svg element to render images and elements
             var svg = d3.select("body")
                 .append("svg")
+                //sets the coordinates
                 .attr("width", 250)
                 .attr("height", 250)
-               
+                 //for circles
                 svg.selectAll("circle")
-                .b(b).enter()
-                .c(c).enter()
+                .data(b).enter()
+                .data(c).enter()
                 .append("circle")
                 .attr("cx", function(d) {return d[0]})
-                .attr("cy", function(d) {return d[1]})
+                .attr("cy", function(e) {return e[0]})
                 .attr("r", 4)
 
             }
